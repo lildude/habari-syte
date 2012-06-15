95e793c52672262a8f6864946702596b20f5a6d8
Colin Seymour <lildood@gmail.com>
2012-06-15 17:14:47 +0100

    Updated header

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title><?php Options::out( 'title' ) ?><?php if ( $request->display_entry && isset( $post ) ) { echo $post->title; } ?></title>
-	<meta name="description" content="<?php Options::get('about'); ?>" />
+	<title><?php Options::out( 'title' ); ?> <?php if ( $request->display_entry && isset( $post ) ) { echo " :: {$post->title}"; } ?></title>
+	<meta name="description" content="<?php Options::get( 'about' ); ?>" />
 	<meta name="keywords" content="Rodrigo Neri, rigoneri, developer, designer, entrepreneur, instin, myhomework" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="<?php Site::out_url( 'theme' ); ?>/imgs/favicon.ico" type="image/x-icon">
@@ -20,19 +20,21 @@
 		<div class="picture">
 			<a href="/" rel="home"></a>
 		</div>
-		<?php // TODO: Make this configurable ?>
-		<h1>Rodrigo Neri</h1>
-		<h2>iOS & Web Developer + Designer + Entrepreneur Co-Founder of <a href="http://instin.com">Instin</a></h2>
+		<h1><?php Options::out( 'title' ); ?></h1>
+		<h2><?php Options::out( 'tagline' ); ?></h2>
 	</hgroup>
   <nav>
 	  <?php // TODO: Make these blocks and extensible by other plugins ?>
     <ul class="main-nav">
-      <li><a href="/" id="home-link">Home</a></li>
+		<li <?php if ( $request->display_home ) { echo 'class="sel"'; } ?>><a href="/" id="home-link"><?php echo _t( 'Home', 'syte'); ?></a></li>
+		<?php //echo $theme->area( 'sidebar' ); ?>
+		<?php /* */ ?>
       <li><a href="http://twitter.com/#!/rigoneri" id="twitter-link">Twitter</a></li>
       <li><a href="http://github.com/rigoneri" id="github-link">Github</a></li>
       <li><a href="http://dribbble.com/rigoneri" id="dribbble-link">Dribbble</a></li>
       <li><a href="http://instagr.am/p/Lau1VrEPbt" id="instagram-link">Instagram</a></li>
       <li><a href="mailto:contact@rigoneri.com?subject=Hello" id="contact-link">Contact</a></li>
+	  <?php /* */ ?>
     </ul>
   </nav>
   <a href="https://github.com/rigoneri/syte" class="fork-me">Fork me on Github</a>
