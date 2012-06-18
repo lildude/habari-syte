bb23e7ca09da4db56ab55d6824b21abd07e08ba1
Colin Seymour <lildood@gmail.com>
2012-06-18 17:47:18 +0100

    Modified sidebar link

--- sidebar.blockwrapper.php
+++ sidebar.blockwrapper.php
@@ -1,2 +1,2 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
-<li><a href="" id="<?php echo strtolower( $block->title ); ?>-link"><?php echo $block->title; ?></a></li>
+<li><a href="<?php echo $block->url; ?>" id="<?php echo strtolower( $block->title ); ?>-link"><?php echo $block->title; ?></a></li>
