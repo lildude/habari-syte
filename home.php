ce0a5bf082d6051aba059f60c9af224f09856281
Colin Seymour <lildood@gmail.com>
2012-06-21 18:20:03 +0100

    Removed unused code

--- home.php
+++ home.php
@@ -6,26 +6,6 @@ $theme->display( 'header' ); ?>
   <span class="loading">loading blog posts ...</span>
 </section>
 */ ?>
-<?php /* implement this:
- <article id="{{ id }}">
-  <hgroup>
-    <h2><a href="/post/{{ id }}">{{ title }}&nbsp;</a></h2>
-    <h3><a href="#{{ id }}">{{ formated_date }}</a></h3>
-  </hgroup>
-  {{{ body }}}
-  {{#if tags}}
-  <footer>
-    <h4>Tags</h4>
-    <ul class="tags">
-      {{#each tags}}
-      <li><a href="/tags/{{ this }}">{{ this }}</a></li>
-      {{/each}}
-    </ul>
-  </footer>
-  {{/if}}
-</article>
-
- */ ?>
 <section class="main-section blog-section" id="blog-posts">
 	<?php foreach ( $posts as $post ) : ?>
 	<article class="post" id="<?php echo $post->id; ?>">
