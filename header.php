93f027557c9ff50024e7b20250d5a95daeffec89
Colin Seymour <lildood@gmail.com>
2012-06-27 17:33:13 +0100

    Tidied up code

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8" />
 	<title><?php Options::out( 'title' ); ?> <?php if ( $request->display_entry && isset( $post ) ) { echo " :: {$post->title}"; } ?></title>
 	<meta name="description" content="<?php Options::get( 'about' ); ?>" />
+	<?php // TODO: Make this configurable within Habari ?>
 	<meta name="keywords" content="Rodrigo Neri, rigoneri, developer, designer, entrepreneur, instin, myhomework" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="<?php Site::out_url( 'theme' ); ?>/imgs/favicon.ico" type="image/x-icon">
@@ -24,17 +25,9 @@
 		<h2><?php Options::out( 'tagline' ); ?></h2>
 	</hgroup>
   <nav>
-	  <?php // TODO: Make these blocks and extensible by other plugins ?>
     <ul class="main-nav">
 		<li <?php if ( $request->display_home ) { echo 'class="sel"'; } ?>><a href="<?php Site::out_url( 'habari' ); ?>/" id="home-link"><?php echo _t( 'Home', 'syte'); ?></a></li>
 		<?php echo $theme->area( 'sidebar' ); ?>
-		<?php /* * ?>
-      <li><a href="http://twitter.com/#!/lildude" id="twitter-link">Twitter</a></li>
-      <li><a href="http://github.com/rigoneri" id="github-link">Github</a></li>
-      <li><a href="http://dribbble.com/rigoneri" id="dribbble-link">Dribbble</a></li>
-      <li><a href="http://instagr.am/p/Lau1VrEPbt" id="instagram-link">Instagram</a></li>
-      <li><a href="mailto:contact@rigoneri.com?subject=Hello" id="contact-link">Contact</a></li>
-	  <?php /* */ ?>
     </ul>
   </nav>
   <a href="https://github.com/rigoneri/syte" class="fork-me">Fork me on Github</a>
