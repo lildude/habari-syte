3ef3fdb8d093ea10247ce91d83352f5967fc98c4
Colin Seymour <lildood@gmail.com>
2012-07-06 14:42:05 +0100

    Added configurable keywords and changed favicon

--- header.php
+++ header.php
@@ -3,13 +3,16 @@
 <head>
 	<meta charset="utf-8" />
 	<title><?php Options::out( 'title' ); ?> <?php if ( $request->display_entry && isset( $post ) ) { echo " :: {$post->title}"; } ?></title>
-	<meta name="description" content="<?php Options::get( 'about' ); ?>" />
-	<?php // TODO: Make this configurable within Habari ?>
-	<meta name="keywords" content="Rodrigo Neri, rigoneri, developer, designer, entrepreneur, instin, myhomework" />
+	<meta name="description" content="<?php Options::out( 'about' ); ?>" />
+	<meta name="keywords" content="<?php ( $request->display_entry && isset ( $post ) && count( $post->tags ) > 0 ) ? $post->tags_out : Options::out( 'keywords' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" href="<?php Site::out_url( 'theme' ); ?>/imgs/favicon.ico" type="image/x-icon">
+<?php if ( User::get_by_name( 'admin' )->info->imageurl != '' ) : ?>
+	<link rel="icon" href="<?php echo User::get_by_name( 'admin' )->info->imageurl; ?>" type="image/png"> 
+	<link rel="shortcut icon" href="<?php echo User::get_by_name( 'admin' )->info->imageurl; ?>" type="image/png">
+<?php else : ?>
+	<link rel="icon" href="<?php Site::out_url( 'theme' ); ?>/imgs/favicon.ico" type="image/x-icon"> 
 	<link rel="shortcut icon" href="<?php Site::out_url( 'theme' ); ?>/imgs/favicon.ico" type="image/x-icon">
-	
+<?php endif; ?>
 	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<link rel="stylesheet/less" type="text/css" href="<?php echo Site::get_url( 'theme' ); ?>/css/less/styles.less" media="screen, projection">
 	<?php echo $theme->header(); ?>
