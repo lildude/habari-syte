21f82be793f7b11e21747ab05e290e2df292e635
Colin Seymour <lildood@gmail.com>
2012-07-06 17:22:11 +0100

    Update comments

--- comments.php
+++ comments.php
@@ -1,7 +1,6 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-		<section id="comments">
-			<a href="<?php echo $post->permalink; ?>#comments"><h4><?php echo $post->comments->moderated->count . _n( ' Comment', ' Comments', $post->comments->moderated->count ); ?></h4></a>
-<?php if ( $post->comments->moderated->count ) : ?>
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } 
+
+if ( $post->comments->moderated->count ) : ?>
     <?php foreach ( $post->comments->comments->moderated as $comment ) :
         $comment_url = ( $comment->url_out == '' ) ? $comment->name_out : '<a href="' . $comment->url_out . '">' . $comment->name_out . '</a>'; ?>
 			
@@ -11,7 +10,7 @@
 					</div>
 					<footer>
 						<?php if ( $loggedin ) : ?><span class="comment-edit"><a href="<?php URL::out( 'admin', 'page=comment&id=' . $comment->id); ?>">&#x2386;</a></span><?php endif; ?>
-						<span class="comment-author"><?php /* &#x21AA; */ ?><?php echo $comment_url; ?></span>
+						<span class="comment-author"><?php echo $comment_url; ?></span>
 						<time datetime="<?php echo $post->pubdate->text_format('{c}'); ?>" class="comment-time"><?php echo $comment->date->out('d M y @ h:m'); ?></time>			
 						<?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) : ?><span class="notice">Your comment is awaiting moderation.</span><?php endif; ?>
 					</footer>
@@ -19,11 +18,9 @@
 	<?php endforeach; ?>
 <?php endif; ?>
 <?php if ( ! $post->info->comments_disabled ) : ?>
-
-<?php if ( Session::has_messages() ) Session::messages_out(); ?>
-			<?php $post->comment_form()->out(); ?>
-	<?php endif; ?>
-		</section>
+	<?php if ( Session::has_messages() ) Session::messages_out(); ?>
+	<?php $post->comment_form()->out(); ?>
+<?php endif; ?>
 
 
 
