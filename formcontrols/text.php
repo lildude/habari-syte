d63101790a499775692cffa30e7f7b90607b9136
Colin Seymour <lildood@gmail.com>
2012-07-06 17:21:59 +0100

    Update formcontrols

--- formcontrols/text.php
+++ formcontrols/text.php
@@ -1,7 +1,23 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
-<p>
-    <input<?php if ( isset( $control_title ) ) { ?> title="<?php echo $control_title; ?>"<?php } else { echo ( isset( $title ) ? " title=\"$title\"" : '' ); } if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } if ( isset( $size ) ) { ?> size="<?php echo $size; ?>"<?php } ?> type="<?php if ( User::identify()->loggedin ) { echo "hidden"; } else { if ( isset( $type ) ) { echo $type; } else { echo "text"; } } ?>" id="<?php echo $field; ?>" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars( $value ); ?>" <?php if ( isset( $required ) ) { ?>required=""<?php } ?> <?php if ( isset( $placeholder ) ) { ?>placeholder="<?php echo $placeholder;?>"<?php } ?>>
-    <?php if ( ! User::identify()->loggedin ) : ?>
-	<label<?php if ( isset( $label_title ) ) { ?> title="<?php echo $label_title; ?>"<?php } else { echo ( isset( $title ) ? " title=\"$title\"" : '' ); } ?> for="<?php echo $field; ?>"><?php echo $this->caption; ?></label>
-	<?php endif; ?>
-</p>
+	<?php /* <input<?php if ( isset( $control_title ) ) { ?> title="<?php echo $control_title; ?>"
+	<?php } else { echo ( isset( $title ) ? " title=\"$title\"" : '' ); } 
+	if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"
+	<?php } if ( isset( $size ) ) { ?> size="<?php echo $size; ?>"<?php } ?> 
+	type="<?php if ( User::identify()->loggedin ) { echo "hidden"; } else { if ( isset( $type ) ) { echo $type; } else { echo "text"; } } ?>" 
+	id="<?php echo $field; ?>" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars( $value ); ?>" 
+	<?php if ( isset( $required ) ) { ?>required=""<?php } ?> 
+	<?php if ( isset( $placeholder ) ) { ?>placeholder="<?php echo $placeholder;?>"<?php } ?>>
+	*/ ?>
+	<input <?php
+		echo $control->parameter_map(
+			array(
+				'title' => array('control_title', 'title'),
+				'tabindex', 'size', 'maxlength', 'type', 'placeholder', 'required',
+				'id' => 'field',
+				'name' => 'field',
+			),
+			array(
+				'value' => Utils::htmlspecialchars( $value ),
+			)
+		);
+		?>>
--- formcontrols/textarea.php
+++ formcontrols/textarea.php
@@ -1,7 +1,12 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
 <p class="comment_box">
-<textarea name="<?php echo $field; ?>" id="<?php echo $field; ?>"<?php echo ( isset( $class ) ? " class=\"$class\"" : '' ) . 
-	" rows=\"" . ( isset( $rows ) ? $rows : 10 ) . "\" cols=\"" . ( isset( $cols ) ? $cols : 100 );
-	if ( isset( $control_title ) ) { echo "\" title=\"$control_title\""; } else { echo ( isset( $title ) ? "\" title=\"$title\"" : '"' ); } if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } ?> <?php if ( isset( $required ) ) { ?>required=""<?php } ?>><?php 
-echo htmlspecialchars( $value ); ?></textarea>
-</p>
+<textarea <?php
+	echo $control->parameter_map(
+		array(
+			'title' => array('control_title', 'title'),
+			'tabindex', 'class', 'required', 'cols', 'rows',
+			'id' => 'field',
+			'name' => 'field',
+		)
+	);
+	?>><?php echo htmlspecialchars( $value ); ?></textarea></p>
