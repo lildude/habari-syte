9a96b67525dc8b98696f5e0f3192bd534a330e1c
Colin Seymour <lildood@gmail.com>
2012-08-03 11:23:12 +0100

    Implemented Dribbble with blocks

--- js/components/dribbble.js
+++ js/components/dribbble.js
@@ -19,35 +19,16 @@ function setupDribbble(url, el) {
      var spinner = new Spinner(spin_opts).spin();
      $('#dribbble-link').append(spinner.el);
 
-	 // site_path is defined in theme.php
-     require(["json!"+site_path+"/dribbble/" + username, "text!templates/dribbble-view.html"],
-        function(dribbble_data, dribbble_view) {
-            if (dribbble_data.message || dribbble_data.length == 0) {
-                window.location = href;
-                return;
-            }
-
-            var template = Handlebars.compile(dribbble_view);
-
-            var user = dribbble_data.shots[0].player;
-            user.following_count = numberWithCommas(user.following_count);
-            user.followers_count = numberWithCommas(user.followers_count);
-            user.likes_count = numberWithCommas(user.likes_count);
-
-            var template_data = {
-                "user": user,
-                "shots": dribbble_data.shots
-            }
-
-            $(template(template_data)).modal().on('hidden', function () {
-                $(this).remove();
-                adjustSelection('home-link');
-            })
-
-            spinner.stop();
-        });
-
-     return;
+	 var href = site_path+"/dribbble/" + username
+		$.get(href, function(data) {
+			$(data).modal().on('hidden', function () {
+				$(this).remove();
+				adjustSelection('home-link');
+			});
+		}).success(function() {
+			spinner.stop();
+		});
+		return;
   }
 
   window.location = href;
