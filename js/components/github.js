82921631bb2b1c256ef17bf3d6436d171e96a35b
Colin Seymour <lildood@gmail.com>
2012-08-03 10:57:14 +0100

    Implemented Github with blocks

--- js/components/github.js
+++ js/components/github.js
@@ -1,47 +1,35 @@
 
 function setupGithub(url, el) {
-  var href = el.href;
+	var href = el.href;
 
-  if ($('#github-profile').length > 0) {
-    window.location = href;
-    return;
-  }
+	if ($('#github-profile').length > 0) {
+		window.location = href;
+		return;
+	}
 
-  var params = url.attr('path').split('/').filter(function(w) {
-      if (w.length)
-          return true;
-      return false;
-  })
+	var params = url.attr('path').split('/').filter(function(w) {
+		if (w.length)
+			return true;
+		return false;
+	})
 
-  if (params.length == 1) {
-     var username = params[0];
+	if (params.length == 1) {
+		var username = params[0];
 
-     var spinner = new Spinner(spin_opts).spin();
-     $('#github-link').append(spinner.el);
+		var spinner = new Spinner(spin_opts).spin();
+		$('#github-link').append(spinner.el);
 
-	 // site_path is defined in theme.php
-     require(["json!"+site_path+"/github/" + username, "text!templates/github-profile-view.html"],
-        function(github_data, github_view) {
-            if (github_data.error || github_data.length == 0) {
-                window.location = href;
-                return;
-            }
+		var href = site_path+"/github/" + username
+		$.get(href, function(data) {
+			$(data).modal().on('hidden', function () {
+				$(this).remove();
+				adjustSelection('home-link');
+			});
+		}).success(function() {
+			spinner.stop();
+		});
+		return;
+	}
 
-            var template = Handlebars.compile(github_view);
-            github_data.user.following = numberWithCommas(github_data.user.following)
-            github_data.user.followers = numberWithCommas(github_data.user.followers)
-
-            $(template(github_data)).modal().on('hidden', function () {
-                $(this).remove();
-                adjustSelection('home-link');
-            })
-
-            spinner.stop();
-
-        });
-
-     return;
-  }
-
-  window.location = href;
+	window.location = href;
 }
