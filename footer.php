beddea0de2514cd5262895b2cd29f44c59fecc6d
Colin Seymour <lildood@gmail.com>
2012-08-15 16:36:48 +0100

    Fixed missing url in footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <span class="nav-btn-bar"></span>
     <span class="nav-btn-bar"></span>
   </span>
-  <h3><a href="/"><?php Site::out_url( 'habari' ); ?></a></h3>
+  <h3><a href="<?php Site::out_url( 'habari' );?>/"><?php Site::out_url( 'habari' ); ?></a></h3>
 </div>
 
 <?php echo $theme->footer(); ?>
